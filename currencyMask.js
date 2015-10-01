66c0188179c6cb1e87a7e85f394cb6bc64265113
gumiz <gumiz@gumiz.pl>
2015-10-01 14:02:45 +0200

    currencyMask

---
+++ currencyMask.js
@@ -0,0 +1,31 @@
+﻿appDirectives.directive('currencyMask', function () {
+    return {
+        restrict: 'A',
+        require: '^ngModel',
+        scope: true,
+        link: function (scope, el, attrs, ngModelCtrl) {
+            var money = {
+                decimals: 2,
+                thousandSeparator: ' ',
+                decimalSeparator: ','
+            };
+
+            function formatter(value) {
+                var properValue = value.toString().replace(',', '.');
+                value = parseFloat(properValue.replace(/[^0-9,.-]/g, ''));
+                var formattedValue = accounting.formatMoney(value, '', money.decimals, money.thousandSeparator, money.decimalSeparator);
+                el.val(formattedValue);
+                return formattedValue;
+            }
+
+            ngModelCtrl.$formatters.push(formatter);
+
+            el.bind('focus', function() {
+            });
+
+            el.bind('blur', function() {
+                formatter(el.val());
+            });
+        }
+    };
+});
